1c448361c987bb89a104c9a3d147672139bbbfab
Travis Gee <geet084@gmail.com>
2018-11-03 10:04:12 -0600

    Add functionality to append cards to page

--- main.js
+++ main.js
@@ -3,6 +3,50 @@ document.querySelector('.form-input').addEventListener('click', getFormInput);
 document.querySelector('.photo-area').addEventListener('click', photoThing);
 
 
+function cl(elem) {
+  return console.log(elem);
+}
+function select(param) {
+  return document.querySelector(param);
+}
+
+
+
+function getFormInput(event) {
+  event.preventDefault();
+  var verifyIt = event.target.classList;
+  
+  event.target.id === 'form-title' ? getTitleInput() : 'Do nothing';
+  event.target.id === 'form-caption' ? getCaptionInput() : 'Do nothing';
+  verifyIt.contains('choose-btn') ? chooseFileBtn() : 'Do nothing';
+  verifyIt.contains('view-fav') ? cl('view something') : 'Do nothing';
+  verifyIt.contains('add-to') ? cl('add to something') : 'Do nothing';
+}
+
+function getTitleInput() {
+  var newTitle =  select('#form-title').value;
+  return newTitle;
+}
+
+function getCaptionInput() {
+  var newCaption = select('#form-caption').value;
+  return newCaption;
+}
+
+function chooseFileBtn() {
+  var photo = new Photo(getTitleInput(), getCaptionInput());
+  var aFoto = new Photo(photo.title, photo.caption, photo.id, photo.file, photo.favorite);
+  var newCard = document.createElement('section');
+  newCard.className = 'foto-card';
+  aFoto.cardInfo(newCard);
+  select('.photo-area').prepend(newCard);
+}
+
+
+
+
+
+
 function photoThing() {
   if (event.target.classList.contains('del-img')) {
     console.log('remove');
@@ -12,33 +56,8 @@ function photoThing() {
 }
 
 
-function getFormInput(event) {
-  if (event.target.id === 'form-title') {
-    getTitleInput();
-  } else if (event.target.id === 'form-caption') {
-    getCaptionInput();
-  } else if (event.target.classList.contains('choose-btn')) {
-    event.preventDefault();
-    console.log('choose file');
-  } else if (event.target.classList.contains('view-fav')) {
-    event.preventDefault();
-    console.log('view things');
-  } else if (event.target.classList.contains('add-to')) {
-    event.preventDefault();
-    console.log('add stuff');
-  }
 
-}
 
-// FOR CAPTION INPUT
-function getCaptionInput() {
-  console.log(document.querySelector('#form-caption').value); 
-}
-
-// FOR TITLE INPUT
-function getTitleInput() {
-  console.log(document.querySelector('#form-title').value); 
-}
 
 // FOR SEARCH BAR
 function getSearchInput() {
--- photo.js
+++ photo.js
@@ -1,10 +1,25 @@
 class Photo {
-  constructor(title, caption, file) {
-    this.id = Date().now;
+  constructor(title, caption, id, file) {
     this.title = title || 'Your Title Here';
     this.caption = caption || 'Your Caption Here';
+    this.id = id || Date.now();
     this.file = file || 'unknown';
     this.favorite = false;
+
+  }
+
+  cardInfo(fotoCard) {
+    fotoCard.innerHTML = 
+    `<section class="card" id=${this.id} data-name=${this.id}>
+      <article class="card-title">${this.title}</article>
+      <article class="card-img ${this.file}"></article>
+      <article class="card-body">${this.caption}</article>
+      <article class="card-btm">
+        <img class="del-img" src="images/delete.svg" alt="">
+        <img class="fav-img ${this.favorite}" src="images/favorite.svg" alt="">
+      </article>
+    </section>
+    `;
 
   }
 
